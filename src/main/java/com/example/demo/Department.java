ec7f6cdf78e32cd559e9f81d67134049b2b9d695
prasantib <prasanti.babu@gmail.com>
2019-12-05 17:15:31 -0500

    employee

---
+++ src/main/java/com/example/demo/Department.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String departmentName;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER)
+    private Set<User> users;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setName(String name) {
+        this.departmentName = departmentName;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.Set;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    @GetMapping("/register")
+    public String showRegistrationPage(Model model) {
+        model.addAttribute("user", new com.example.demo.User());
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationPage(@Valid
+                                          @ModelAttribute("user") User user, BindingResult result,
+                                          Model model) {
+        model.addAttribute("user", user);
+        if (result.hasErrors())
+        {
+            return "registration";
+        }
+        else
+        {
+            //userService.saveUser(user);
+            model.addAttribute("message", "User Account Created");
+        }
+        return "index";
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model){
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
+        return "secure";
+    }
+}
